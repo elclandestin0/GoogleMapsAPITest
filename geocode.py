06526ef5ec469bfaaaf2ed974ebf782252218b88
mk <majd.khoury93@gmail.com>
2018-01-12 20:00:25 -0500

    Added location finding function in geocode that returns a JSON-based text

---
+++ geocode.py
@@ -0,0 +1,15 @@
+import httplib2, json
+
+def fetchGeocodeLocation(input):
+    # Unsafe to use API key in vulnerable code, but for this file it's fine.
+    # It is just to demonstrate using python's server side to fetch locations.
+    google_api_key = "AIzaSyA-pnTIuJqwtfsInE9n1slQictD0FdJjNI"
+    string = input.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (string, google_api_key))
+    h = httplib2.Http()
+    response, content = h.request(url, 'GET')
+    result = json.loads(content)
+    return result
+    # return result
+
+fetchGeocodeLocation("Dallas, Texas")
